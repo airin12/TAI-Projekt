bb1660678d6fba87d805e2f0402e3f7f9e679c61
palys <tomek201992@gmail.com>
2014-12-11 11:41:16 +0100

    Dodane javadoci dla dao

--- src/main/java/pl/edu/agh/analizer/youtube/dao/UserDao.java
+++ src/main/java/pl/edu/agh/analizer/youtube/dao/UserDao.java
@@ -2,11 +2,28 @@ package pl.edu.agh.analizer.youtube.dao;
 
 import pl.edu.agh.analizer.youtube.model.User;
 
+
 public interface UserDao {
 
+	
+	/**
+	 * Inserts user into database 
+	 * 
+	 * @param user user to insert
+	 * @return number of rows inserted
+	 */
 	int insertUser(User user);
 	
+	/**
+	 * Deletes user with specified name 
+	 * @param name name of user to dalete
+	 */
 	void deleteUser(String name);
 	
+	/**
+	 * Selects user with specified name
+	 * @param name name of user
+	 * @return selected user or null if user does not exist
+	 */
 	User selectUser(String name);
 }
